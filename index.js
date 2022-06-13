c4582a600de3acc09fe7c4e8fdac2bead9d025ad
Genci23 <genckaziaj@yahoo.com>
2022-06-14 00:17:06 +0200

    First Salary

--- index.js
+++ index.js
@@ -9,18 +9,19 @@ console.log(taxableAmount);
 
 let personIsTaxed = undefined;
 
-if (yourGrossSalary >= 0 && yourGrossSalary < 80) {
+if (taxableAmount >= 0 && taxableAmount < 80) {
   personIsTaxed = 0 / 100;
-} else if (yourGrossSalary >= 80 && yourGrossSalary < 250) {
-  personIsTaxed = 4 / 100;
-} else if (yourGrossSalary >= 250 && yourGrossSalary < 450) {
-  personIsTaxed = 8 / 100;
-} else if (yourGrossSalary >= 450) {
-  personIsTaxed = 10 / 100;
+} else if (taxableAmount >= 80 && taxableAmount < 250) {
+  personIsTaxed = (4 / 100) * (taxableAmount - 80);
+} else if (taxableAmount >= 250 && taxableAmount < 450) {
+  personIsTaxed = (4 / 100) * 170 + (8 / 100) * (taxableAmount - 250);
+} else if (taxableAmount >= 450) {
+  personIsTaxed =
+    (4 / 100) * 170 + (8 / 100) * 200 + (10 / 100) * (taxableAmount - 450);
 }
 
 console.log(personIsTaxed);
 
-let takeHomePay = taxableAmount - personIsTaxed * taxableAmount;
+let takeHomePay = taxableAmount - personIsTaxed;
 
 console.log(takeHomePay);
